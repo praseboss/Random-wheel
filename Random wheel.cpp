b99e07b204a244cb9d5257f4c4499ce9102053e5
Luka Pustijanac <luka.pustijanac@gmail.com>
2025-05-13 11:52:33 +0200

    Add files via upload

---
+++ Random wheel.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+#include <string>
+
+void waitForSeconds(int seconds) {
+    std::this_thread::sleep_for(std::chrono::seconds(seconds));
+}
+int main() {
+    int n;
+    std::cout << "RANDOM WHEEL, input amount of options: ";
+    std::cin >> n;
+    std::vector<std::string> options(n);
+    std::cout << "Enter " << n << " names:\n";
+    std::cin.ignore();
+    for (int i = 0; i < n; ++i) {
+        std::getline(std::cin, options[i]);
+    }
+    std::cout << "Selecting random, wait for 3 seconds...\n";
+    waitForSeconds(3);
+    std::srand(std::time(0));
+    int g = std::rand() % n;
+    std::cout << "The selected option is: " << options[g] << std::endl;
+    return 0;
+}
